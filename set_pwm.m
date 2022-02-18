11c165f67386b7f9cf5bfbc88dbf4feba7e5642d
Emily Corson <corson12@students.rowan.edu>
2022-02-18 13:10:19 -0500

    Fixed PWM File

--- set_pwm.m
+++ set_pwm.m
@@ -1,4 +1,4 @@
-function action = set_pwm(device, pwm_value)
+function set_pwm(device, pwm_value)
 %% Sets the PWM of the fan
 % Inputs:
 %  ~ device: serialport object controlling the real world system
@@ -8,13 +8,13 @@ function action = set_pwm(device, pwm_value)
 %  ~ action: the control action to change the PWM
 %
 % Created by:  Kyle Naddeo 2/4/2022
-% Modified by: YOUR NAME and DATE
+% Modified by: Emily Corson 2/18/2022
 
 %% Force PWM value to be valid
-% pwm_value = % Bound value to limits 0 to 4095
+%pwm_value = 0:4095;
 
 %% Send Command
-% action = % string value of pwm_value
+writeline(device, "P" + pwm_value);
 % use the serialport() command options to change the PWM value to action
 
 end
